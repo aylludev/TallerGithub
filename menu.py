db2b8b4ad86ecb37865e2d0ecae03f4ddda8da36
aylludev <cafo1995@hotmail.com>
2025-09-11 23:25:52 -0500

    primer comit colaborador

---
+++ menu.py
@@ -0,0 +1,10 @@
+print('##################################################################')
+print('#                  PROGRAMA QUE AYUDA A APRENDER GIT             #')
+print('##################################################################')
+
+option = 1
+
+while option > '0':
+    print('0. SALIR')
+    option = input('Ingrese una opcion: ')
+
